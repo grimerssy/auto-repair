2859fcc1a1ae11e76e708ceeeb399044f67d40ca
Stanislav Stoyanov <grimerssy@gmail.com>
2022-10-02 00:01:57 +0300

    add services endpoints

--- server/src/api/mod.rs
+++ server/src/api/mod.rs
@@ -1,3 +1,5 @@
+pub mod services;
+
 use actix_web::web::Data;
 use diesel::result::Error;
 
--- server/src/routing.rs
+++ server/src/routing.rs
@@ -1,4 +1,6 @@
 use crate::api::*;
 use actix_web::web;
 
-pub fn configuration(cfg: &mut web::ServiceConfig) {}
+pub fn configuration(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/api").service(web::scope("/services").service(services::get_all)));
+}
