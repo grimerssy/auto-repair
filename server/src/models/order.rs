48ec8fab4247d3a2a4baee669b0968c7b3a30755
Stanislav Stoyanov <grimerssy@gmail.com>
2022-10-02 00:47:31 +0300

    fix order model, add RawOrder

--- server/src/models/order.rs
+++ server/src/models/order.rs
@@ -1,13 +1,24 @@
 use diesel::Queryable;
 use serde::Serialize;
 
-use super::timestamp::Timestamp;
+use super::{contact::Contact, service::Service, timestamp::Timestamp};
+
+#[derive(Queryable)]
+pub struct RawOrder {
+    pub id: i32,
+    pub contact_id: i32,
+    pub service_id: i32,
+    pub start_time: Timestamp,
+    pub car_make: String,
+    pub car_model: String,
+    pub car_year: i16,
+}
 
 #[derive(Queryable, Serialize)]
 pub struct Order {
     pub id: i32,
-    pub contact_id: i32,
-    pub service_id: i32,
+    pub contact: Contact,
+    pub service: Service,
     pub start_time: Timestamp,
     pub car_make: String,
     pub car_model: String,
