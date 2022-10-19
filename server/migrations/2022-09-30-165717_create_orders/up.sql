3882dd518d214058841a3a7bb61430dd757dc219
Stanislav Stoyanov <grimerssy@gmail.com>
2022-10-19 15:45:21 +0300

    fix delete behaviour in migration

--- server/migrations/2022-09-30-165717_create_orders/up.sql
+++ server/migrations/2022-09-30-165717_create_orders/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    contact_id INTEGER NOT NULL REFERENCES contacts(id),
-    service_id INTEGER NOT NULL REFERENCES services(id),
+    contact_id INTEGER NOT NULL REFERENCES contacts(id) ON DELETE CASCADE,
+    service_id INTEGER NOT NULL REFERENCES services(id) ON DELETE CASCADE,
     start_time TIMESTAMP NOT NULL,
     car_make VARCHAR(32) NOT NULL,
     car_model VARCHAR(32) NOT NULL,
